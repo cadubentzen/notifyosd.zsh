36c902ca3785034f7fddda0cd91384d638682180
cadubentzen <cadubentzen@gmail.com>
2017-12-11 16:08:22 -0300

    make script simpler and add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Notifications on terminal commands
+
+This script trows a notification for any terminal commands that takes longer than 10 seconds to complete.
+
+## Usage:
+
+Add `source $NOTIFYOSDFOLDER/notifyosd.zsh` to the end of your .zshrc, where `$NOTIFYOSDFOLDER` is the folder you placed the script.
--- notifyosd.zsh
+++ notifyosd.zsh
@@ -14,15 +14,6 @@ function notifyosd-precmd() {
             cmd_end=`date +%s`
             ((cmd_secs=$cmd_end - $cmd_start))
         fi
-        if [ $retval -gt 0 ]; then
-			cmdstat="with warning"
-			sndstat="/usr/share/sounds/gnome/default/alerts/sonar.ogg"
-			urgency="critical"
-		else
-            cmdstat="successfully"
-			sndstat="/usr/share/sounds/gnome/default/alerts/glass.ogg"
-			urgency="normal"
-        fi
         if [ ! -z "$cmd" -a $cmd_secs -gt 10 ]; then
 			if [ $gnuunits -gt 0 ]; then
 				cmd_time=$(units "$cmd_secs seconds" "centuries;years;months;weeks;days;hours;minutes;seconds" | \
@@ -32,12 +23,10 @@ function notifyosd-precmd() {
 			fi
             if [ ! -z $SSH_TTY ] ; then
                 notify-send -i utilities-terminal \
-						-u $urgency "$cmd_basename on `hostname` completed $cmdstat" "\"$cmd\" took $cmd_time"; \
-						play -q $sndstat
+						"$cmd_basename on `hostname` completed" "\"$cmd\" took $cmd_time"; \
             else
                 notify-send -i utilities-terminal \
-						-u $urgency "$cmd_basename completed $cmdstat" "\"$cmd\" took $cmd_time"; \
-						play -q $sndstat
+						"$cmd_basename completed" "\"$cmd\" took $cmd_time"; \
             fi
         fi
         unset cmd
